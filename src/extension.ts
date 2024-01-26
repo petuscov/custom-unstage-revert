75eaa647f0f68a835ffaaa7a82e7ea574b7a7936
petuscov <petuscov@gmail.com>
2024-01-26 22:03:33 +0100

    Commit inicial.

---
+++ src/extension.ts
@@ -0,0 +1,33 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "custom-unstage-revert" is now active!');
+
+// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposableUnstage = vscode.commands.registerCommand('custom-unstage-revert.unstage', () => {
+		// The code you place here will be executed every time your command is executed
+		// Display a message box to the user
+		vscode.window.showInformationMessage('TO-DO UNSTAGE TAKING REGEXPS!');
+	});
+
+	let disposableRevert = vscode.commands.registerCommand('custom-unstage-revert.revert', () => {
+		// The code you place here will be executed every time your command is executed
+		// Display a message box to the user
+		vscode.window.showInformationMessage('TO-DO REVERT TAKING REGEXPS!');
+	});
+
+	context.subscriptions.push(disposableUnstage);
+	context.subscriptions.push(disposableRevert);
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
